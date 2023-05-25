5209867339ed6a861d68740cd596fc9df0463fc9
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-05-25 10:36:17 +0200

    #

--- exo_etudiant.sql
+++ exo_etudiant.sql
@@ -18,6 +18,14 @@ WHERE evaluer.id_etudiant = etudiant.id_etudiant  -- où l'id etudiant de la tab
 AND evaluer.id_matiere = matiere.id_matiere  -- et où l'id matière de la table evaluer correspond à l'id matière de la table matière.
 GROUP BY etudiant.id_etudiant, matiere.id_matiere -- et on fait cette opération pour chaque étudiant, pour chaque matière. On regroupe tous les étudiants du même ID et toutes les matières du même ID. Ce qui permet de sortir une moyenne
 
+-- Moyenne par matière
+
+CREATE OR REPLACE VIEW moyenne_matiere AS 
+SELECT matiere.libelle_mat, AVG(evaluer.note) -- On veut afficher le libelle de la matière ainsi qu'une moyenne de notes
+FROM matiere, evaluer -- depuis les tables matiere et evaluer
+WHERE matiere.id_matiere = evaluer.id_matiere -- on fait le lien entre les id matiere des 2 tables
+GROUP BY matiere.libelle_mat -- Et on regroupe par matière ce qui permettra de faire une moyenne par matière.
+
 -- Moyenne générale de chaque étudiant
 
 CREATE OR REPLACE VIEW moyenne_etudiant AS -- Création d'une view moyenne_etudiant
@@ -25,3 +33,10 @@ SELECT etudiant.nom, AVG(evaluer.note) -- On demande le nom ainsi qu'une moyenne
 FROM etudiant, evaluer -- à partir des bases étudiant et évaluer
 WHERE etudiant.id_etudiant = evaluer.id_etudiant -- en faisant le lien entre la table etudiant et evaluer
 GROUP BY etudiant.nom -- et on fait cette opération pour chaque étudiant. C'est ce qui permet d'afficher la moyenne pour 1 étudiant donné.
+
+-- Moyenne de la promo
+
+CREATE OR REPLACE VIEW moyenne_promo AS
+SELECT AVG(evaluer.note)
+FROM evaluer
+
